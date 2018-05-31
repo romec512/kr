2cd27266392e149a78fb357a91a9e1da0f75762d
romec512 <roma-asadov@mail.ru>
2018-05-31 13:33:18 +0300

    Добавьте файлы проекта.

---
+++ kr/Faculty.cpp
@@ -0,0 +1,14 @@
+#include "Faculty.h"
+
+
+
+Faculty::Faculty()
+{
+
+}
+
+
+Faculty::~Faculty()
+{
+
+}
---
+++ kr/Faculty.h
@@ -0,0 +1,10 @@
+#pragma once
+class Faculty
+{
+public:
+	Faculty();
+	~Faculty();
+private:
+
+};
+
